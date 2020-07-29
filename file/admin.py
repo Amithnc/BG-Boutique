ce3e7cc140791d84dd8f538de5d1a9c16145bf5c
Amithnc <53530365+Amithnc@users.noreply.github.com>
2020-07-30 03:24:57 +0530

    updated admin view

--- file/admin.py
+++ file/admin.py
@@ -4,10 +4,10 @@ from django import forms
 
 admin.site.index_title="add files"
 admin.site.site_title="BG-Portal"
-admin.site.site_header="BG PORTAL" 
+admin.site.site_header="BG ADMIN PORTAL" 
      
 class FilesAdmin(admin.ModelAdmin):
-    list_display = ('name','last_updated_on','uploaded_file','updated_by')
+    list_display = ('name','id','last_updated_on','uploaded_file','updated_by')
 
     def save_model(self, request, obj, form, change):
         obj.updated_by = request.user
--- file/views.py
+++ file/views.py
@@ -1,33 +1,37 @@
-from django.shortcuts import render,get_object_or_404,HttpResponseRedirect,redirect
+from django.shortcuts import render,get_object_or_404,redirect
 from .models import Files
 from django.contrib.auth.models import User
 from authority.models import Permission
 from django.contrib import auth
 from .forms import FilesForm 
 from django.contrib.auth.decorators import login_required
-from django.urls import reverse
-import urllib
+from django.db.models import Q
+
 
 
 
 @login_required(login_url='/log')
 def fileview(request):
-    view_obj_id=[]
-    change_obj_id=[]
-    u=request.user
-    usrid=0
-    m=User.objects.filter(username=u)
-    for i in m:
-        usrid=i.id  
-    a=Permission.objects.filter(codename='change',user_id=usrid)
-    b=Permission.objects.filter(codename='view',user_id=usrid)
-    for i in a:
-        change_obj_id.append(i.object_id)     
-    for i in b:
-        view_obj_id.append(i.object_id) 
+    if request.user.is_superuser:
+        filteredchange=Files.objects.all()
+        filteredview=Files.objects.all()
+    else:
+        view_obj_id=[]
+        change_obj_id=[]
+        u=request.user
+        usrid=0
+        m=User.objects.filter(username=u)
+        for i in m:
+            usrid=i.id  
+        a=Permission.objects.filter(Q(codename='change')|Q(codename='change&view'),user_id=usrid,approved=1)
+        b=Permission.objects.filter(Q(codename='view')|Q(codename='change&view'),user_id=usrid,approved=1)
+        for i in a:
+            change_obj_id.append(i.object_id)     
+        for i in b:
+            view_obj_id.append(i.object_id) 
+        filteredchange=Files.objects.filter(id__in=change_obj_id)
+        filteredview=Files.objects.filter(id__in=view_obj_id)
 
-    filteredchange=Files.objects.filter(id__in=change_obj_id)
-    filteredview=Files.objects.filter(id__in=view_obj_id)
     context={}
     context['view']=filteredview
     context['changes']=filteredchange
